c061e1a173409a38954a4576895af8100a93e225
Valentin <valio953@gmail.com>
2017-01-31 11:39:25 +0100

    31.1.17

---
+++ header.php
@@ -0,0 +1,57 @@
+﻿<header>
+    <div class="row">
+        <div class="col-md-6">
+            <img src="images/logosmall.png" alt="logo" />
+        </div>
+        <div class="col-md-6 text-right header-top-buttons">
+            <figure>
+                <a href="#">
+                    <i class="glyphicon glyphicon-user"></i>
+                    Logon to account
+                </a>
+            </figure>
+            <figure>
+                <a href="#">
+                    <i class="glyphicon glyphicon-envelope"></i>
+                    WebMail
+                </a>
+            </figure>
+        </div>
+    </div>
+    
+    
+    <nav class="navbar navbar-default" role="navigation">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+        </div>
+      
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse nopadding" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="#">Account</a></li>
+                <li><a href="#">Domain</a></li>
+                <li><a href="#">Support</a></li>
+                <li><a href="#">Information</a></li>
+                <li class="no-border"><a href="#">Webmail</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <form class="navbar-form" role="search">
+                    <span>Search domain</span>
+                    <div class="input-group">
+                        <input type="text" class="form-control" placeholder="www.yourdomain.com" name="q">
+                        <div class="input-group-btn">
+                            <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+                        </div>
+                    </div>
+                </form>
+            </ul>
+        </div><!-- /.navbar-collapse -->
+    </nav>
+</header>
---
+++ index.php
@@ -0,0 +1,42 @@
+﻿<!DOCTYPE html>
+<html>
+    <head>
+        <!--[if IE 8]>
+            <link HREF="/IE8.css" REL="StyleSheet" TYPE="text/css">
+        <![endif]-->
+    
+        <meta name="norton-safeweb-site-verification" content="xo9qbg4a-l5zi50qameyixskmht7-p257-popwtc3b7-n1jwyvzpbfh6gyps6r-kjbhcbne6jncqc1wzzavrvsdujbxrsfi68gnz29af35abe4yrh9l9kqowla4ni0m-"/>
+        <meta name="description" content="Webhosting Webhotel, Danmarks bedste webhotel. Vi tilbyder kvalitets webhoteller og domæner til lave priser."/>
+        <meta name="keywords" content="Webhosting, webhotel, webhoteller, domæner, WordPress, Joomla, Magento"/>	
+        <meta name="robots" content="index,follow"/>
+        <meta name="GOGGLEBOT" content="index,follow"/>
+        <meta http-equiv="imagetoolbar" content="no"/>
+        <meta name="verify-v1" content="h+NnTjzEHM+4ipnqh7kNa9Neg3zvHIbli1XRskGmdYU=" />
+        <meta name="revisit" content="4 days"/>
+        <meta name="Copyright" content="Webmaster"/>
+        <meta name="Language" content="Danish, English"/>
+        <meta name="distribution" content="global"/>
+        <meta name="copyright" content="webhosting.dk" />
+        <meta name="rating" content="General"/>
+        <meta name="robots" content="ALL"/>
+        <meta name="distribution" content="global"/>	
+        <meta name="author" content="webhosting.dk"/>
+        <meta http-equiv="Content-language" content="da"/>
+        <meta name="DC.title" content="WebHosting A/S - Webhotel, billigt, godt og effektivt"/>
+        <meta name="DC.description" content="webhotel, domain, domæne,webhosting, billigt, gratis, lavpris, autoresponder, email, imap, pop3, smtp, server, virtual, registrering, registration, com,org,net,info,dk,internet,mailhotel,php,php4,asp,frontpage, drupal,magento"/>
+        <meta name="DC.subject" content="Lavpris domains, webhosting siden 1996, Joomla 1 klik installation, bedste service"/>
+        <meta name="DC.coverage.placeName" content="Danmark"/>	
+        <meta name="DC.language" content="da"/>
+        
+        <link rel="stylesheet" type="text/css" href="css/reset.css" />
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<link rel="stylesheet" type="text/css" href="css/style.css" />
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        
+        <title>Webhosting Webhotel  |  Webhosting.dk</title>
+    </head>
+    <body>
+        <?php include_once("header.php");?>
+    </body>
+</html>
