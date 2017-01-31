31c4b3c531b5ab4de3809c28dd88e8821bac931b
Norbert Nagy <nagy.nbt@gmail.com>
2017-01-31 12:58:43 +0100

    norby update

--- index.php
+++ index.php
@@ -4,10 +4,10 @@
         <!--[if IE 8]>
             <link HREF="/IE8.css" REL="StyleSheet" TYPE="text/css">
         <![endif]-->
-    
+
         <meta name="norton-safeweb-site-verification" content="xo9qbg4a-l5zi50qameyixskmht7-p257-popwtc3b7-n1jwyvzpbfh6gyps6r-kjbhcbne6jncqc1wzzavrvsdujbxrsfi68gnz29af35abe4yrh9l9kqowla4ni0m-"/>
         <meta name="description" content="Webhosting Webhotel, Danmarks bedste webhotel. Vi tilbyder kvalitets webhoteller og domæner til lave priser."/>
-        <meta name="keywords" content="Webhosting, webhotel, webhoteller, domæner, WordPress, Joomla, Magento"/>	
+        <meta name="keywords" content="Webhosting, webhotel, webhoteller, domæner, WordPress, Joomla, Magento"/>
         <meta name="robots" content="index,follow"/>
         <meta name="GOGGLEBOT" content="index,follow"/>
         <meta http-equiv="imagetoolbar" content="no"/>
@@ -19,24 +19,154 @@
         <meta name="copyright" content="webhosting.dk" />
         <meta name="rating" content="General"/>
         <meta name="robots" content="ALL"/>
-        <meta name="distribution" content="global"/>	
+        <meta name="distribution" content="global"/>
         <meta name="author" content="webhosting.dk"/>
         <meta http-equiv="Content-language" content="da"/>
         <meta name="DC.title" content="WebHosting A/S - Webhotel, billigt, godt og effektivt"/>
         <meta name="DC.description" content="webhotel, domain, domæne,webhosting, billigt, gratis, lavpris, autoresponder, email, imap, pop3, smtp, server, virtual, registrering, registration, com,org,net,info,dk,internet,mailhotel,php,php4,asp,frontpage, drupal,magento"/>
         <meta name="DC.subject" content="Lavpris domains, webhosting siden 1996, Joomla 1 klik installation, bedste service"/>
-        <meta name="DC.coverage.placeName" content="Danmark"/>	
+        <meta name="DC.coverage.placeName" content="Danmark"/>
         <meta name="DC.language" content="da"/>
-        
+
         <link rel="stylesheet" type="text/css" href="css/reset.css" />
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 		<link rel="stylesheet" type="text/css" href="css/style.css" />
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-        
+
         <title>Webhosting Webhotel  |  Webhosting.dk</title>
     </head>
     <body>
         <?php include_once("header.php");?>
+
+    <section id="products">
+                <div class="col-centered col-lg-12">
+                  <div class="pricing-tables attached ">
+                    <div class="row ">
+                      <div class="col-sm-3 col-md-3 col-lg-3">
+                        <div class="plan  first">
+                          <div class="head">
+                            <h2>Domain</h2>
+                          </div>
+                          <ul class="item-list">
+                            <li>
+                              <strong> Buliding  </strong>One time scraping</li>
+                            <li>
+                              <strong>$xxx4xxx</strong></li>
+                            <li>
+                              <strong>NIL</strong> </li>
+                            <li>
+                              <strong>Third party services</strong> As per use, will be added to bill</li>
+                          </ul>
+                          <div class="price mediafix">
+                            <h4>From only</h4>
+                            <h3>44 <span>DKK</span></h3>
+                            <h4>for a .DK domain</h4>
+                          </div>
+                          <a class="btn-clean btn-pad" href="index.html#contact">Order Now <i class="ion-ios-unlocked-outline"></i></a>
+                        </div>
+                      </div>
+
+                      <div class="col-sm-3 col-md-2 col-lg-2">
+                        <div class="plan first recommended">
+                          <div class="head">
+                            <h2>PHP Basic</h2>
+                          </div>
+                          <ul class="item-list">
+                            <li>
+                              <strong> Buliding  </strong>One time scraping</li>
+                            <li>
+                              <strong>$xxx4xxx</strong></li>
+                            <li>
+                              <strong>NIL</strong> </li>
+                            <li>
+                              <strong>Third party services</strong> As per use, will be added to bill</li>
+                          </ul>
+                          <div class="price">
+                            <h4>From under</h4>
+                            <h3>9.04 <span>DKK</span></h3>
+                            <h4>per month</h4>
+                          </div>
+                          <a class="btn-clean btn-pad" href="index.html#contact">Order Now <i class="ion-ios-unlocked-outline"></i></a>
+                        </div>
+                      </div>
+                      <div class="col-sm-3 col-md-2 col-lg-2">
+                        <div class="plan recommended">
+                          <div class="head">
+                            <h2>PHP Medium</h2>
+                          </div>
+                          <ul class="item-list">
+                            <li>
+                              <strong> Buliding  </strong>One time scraping</li>
+                            <li>
+                              <strong>$xxx4xxx</strong></li>
+                            <li>
+                              <strong>NIL</strong> </li>
+                            <li>
+                              <strong>Third party services</strong> As per use, will be added to bill</li>
+                          </ul>
+                          <div class="price">
+                            <h4>From under</h4>
+                            <h3>24.98 <span>DKK</span></h3>
+                            <h4>per month</h4>
+                          </div>
+                          <a class="btn-clean btn-pad" href="index.html#contact">Order Now <i class="ion-ios-unlocked-outline"></i></a>
+                        </div>
+                      </div>
+                      <div class="col-sm-3 col-md-2 col-lg-2">
+                        <div class="plan last recommended">
+                          <div class="head">
+                            <h2>PHP Business</h2>
+                          </div>
+                          <ul class="item-list">
+                            <li>
+                              <strong> Buliding  </strong>One time scraping</li>
+                            <li>
+                              <strong>$xxx4xxx</strong></li>
+                            <li>
+                              <strong>NIL</strong> </li>
+                            <li>
+                              <strong>Third party services</strong> As per use, will be added to bill</li>
+                          </ul>
+                          <div class="price">
+                            <h4>From under</h4>
+                            <h3>49.88 <span>DKK</span></h3>
+                            <h4>per month</h4>
+                          </div>
+                          <a class="btn-clean btn-pad" href="index.html#contact">Order Now <i class="ion-ios-unlocked-outline"></i></a>
+                        </div>
+                      </div>
+                      <div class="col-sm-12 col-md-3 col-lg-3">
+                        <div class="plan last">
+                          <div class="head value">
+                            <h2>Mail Hosting</h2>
+                          </div>
+                          <ul class="item-list">
+                            <li>
+                              <strong> Buliding  </strong>One time scraping</li>
+                            <li>
+                              <strong>$xxx4xxx</strong></li>
+                            <li>
+                              <strong>NIL</strong> </li>
+                            <li>
+                              <strong>Third party services</strong> As per use, will be added to bill</li>
+                          </ul>
+                          <div class="price">
+                            <h4>From under</h4>
+                            <h3>10 <span>DKK</span></h3>
+                            <h4>per month</h4>
+                          </div>
+                          <a class="btn-clean btn-pad" href="index.html#contact">Order Now <i class="ion-ios-unlocked-outline"></i></a>
+                        </div>
+                      </div>
+                    </div>
+                    <!-- row-->
+                  </div>
+                  <!-- pricing-tables -->
+                </div>
+                <br>
+
+              </section>
+
     </body>
 </html>
