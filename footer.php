824c42f80910bd4320a4a335c3c9d912ee9c0824
Valentin <valio953@gmail.com>
2017-02-02 12:45:16 +0100

    some fixes

---
+++ footer.php
@@ -0,0 +1,25 @@
+﻿<footer>
+        <div class="row" id="stfooter">
+          <div class="col-lg-6  col-md-6 col-sm-6">
+          <ul>
+              <li><a href="http://support.webhosting.dk/ENG" target="_blank">Support section</a></li>
+              <li><a href="http://www.webhosting.dk/ENG/createaccount.php" target="_blank">Create account</a></li>
+              <li><a href="http://www.webhosting.dk/ENG/createoffernew.php" target="_blank">View pricelist</a></li>
+              <li><a href="http://www.webhosting.dk/ENG/createoffernew.php" target="_blank">Create order</a></li>
+              <li><a href="http://www.webhosting.dk/ENG/accountsend.php" target="_blank">Forgot password ?</a></li>
+              <li><a href="http://www.webhosting.dk/ENG/sendemail.php" target="_blank">Contact us</a></li>
+         </ul>
+         </div>
+         <div class="col-lg-6  col-md-6 col-sm-6">
+           <p> Social networking</p>
+           <a href="http://www.facebook.com/webhosting.dk" target="_blank"><img src="images/fb.png" alt=""></a>
+           <a href="https://twitter.com/webhostingdk" target="_blank"><img src="images/twitter.png" alt=""></a>
+         </div>
+       </div>
+       <div class="row">
+         <div class="col-lg-12">
+         <p>WebHosting A/S, CVR 25674138, © 1996 - <?php echo date("Y");?></p>
+         <p>© VN media</p>
+        </div>
+      </div>
+</footer>
--- index.php
+++ index.php
@@ -69,7 +69,7 @@
                             </li>
                         </ul>
                         <p>
-                            To view our price list please click this <a href="#">link</a>.
+                            To view our price list please click this <a href="#products">link</a>.
                         </p>
                     </div>
                     <div class="info-box-absolute">
@@ -231,32 +231,7 @@
                 </div>
             </div>
         </section>
-
-      <footer>
-        <div class="row" id="stfooter">
-          <div class="col-lg-6  col-md-6 col-sm-6">
-          <ul>
-              <li><a href="http://support.webhosting.dk/ENG" target="_blank">Support section</a></li>
-              <li><a href="http://www.webhosting.dk/ENG/createaccount.php" target="_blank">Create account</a></li>
-              <li><a href="http://www.webhosting.dk/ENG/createoffernew.php" target="_blank">View pricelist</a></li>
-              <li><a href="http://www.webhosting.dk/ENG/createoffernew.php" target="_blank">Create order</a></li>
-              <li><a href="http://www.webhosting.dk/ENG/accountsend.php" target="_blank">Forgot password ?</a></li>
-              <li><a href="http://www.webhosting.dk/ENG/sendemail.php" target="_blank">Contact us</a></li>
-         </ul>
-         </div>
-         <div class="col-lg-6  col-md-6 col-sm-6">
-           <p> Social networking</p>
-           <a href="http://www.facebook.com/webhosting.dk" target="_blank"><img src="images/fb.png" alt=""></a>
-           <a href="https://twitter.com/webhostingdk" target="_blank"><img src="images/twitter.png" alt=""></a>
-         </div>
-       </div>
-       <div class="row">
-         <div class="col-lg-12">
-         <p>WebHosting A/S, CVR 25674138, © 1996 - <?php echo date("Y");?></p>
-         <p>© VN media</p>
-        </div>
-      </div>
-    </footer>
-
+        
+        <?php include_once("footer.php");?>
     </body>
 </html>
